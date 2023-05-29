8092a4e237d4016eeafbccfa6ff97b7238f6b871
Andres Zubizarreta <113726288+andreszubi@users.noreply.github.com>
2023-05-29 09:22:51 +0200

    Arrays

---
+++ arraysTest.ts
@@ -0,0 +1,9 @@
+const carMakers = ['ford', 'toyota', 'chevy'];
+const dates = [new Date(), new Date()];
+
+const carsByMake: string[][] = [];
+
+// Help with inference when extracting values
+const car = carMakers[0];
+const myCar = carMakers.pop();
+
